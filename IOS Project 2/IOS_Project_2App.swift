9d23e3d5104a3a0f91e4bd8a5c3cc4bf4dfec438
BT_C09_01 <mobileappdev@bths.edu>
2026-04-28 14:41:02 -0400

    Initial Commit

---
+++ IOS Project 2/IOS_Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Project_2App.swift
+//  IOS Project 2
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

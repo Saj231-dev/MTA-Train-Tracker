5832842548545fd9772d9e3ca73db1bc6350b36a
BT_C09_01 <mobileappdev@bths.edu>
2026-04-30 14:42:18 -0400

    added datamanager class

---
+++ IOS Project 2/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  IOS Project 2
+//
+//  Created by Student on 4/30/26.
+//
+
+import SwiftUI
+
+@Observable
+class DataManager {
+    private let apiURL = "https://api-endpoint.mta.info/"
+    private let apiEndpointL = "Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l"
+}
